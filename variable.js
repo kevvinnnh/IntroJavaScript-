f3094d5decbc24f09a782d326f6ecaecefdf91a8
Kevin Hernandez <84102365+kevvinnnh@users.noreply.github.com>
2022-07-07 10:23:10 -0500

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,18 @@
+// VERY EASY 
+var num1 = 8; 
+var num2 =9;
+
+console.log( `The difference between ${num1} and ${num2} is ${num1-num2}`);
+
+//EASY 
+
+var person1 = "Noad"; 
+var person2 = "Kevin"; 
+
+if ( person1.length > person2.length){
+    console.log(`The name ${person1} is longer than ${person2} by ${person1.length - person2.length} characters.`);
+}
+else{
+    console.log(`The name ${person2} is longer than ${person1} by ${person2.length - person1.length} characters.`);
+}
+
